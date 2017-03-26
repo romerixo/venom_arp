e48278cfcf72ff2deda26bdf085efd3b21f0f9ea
Juan Antonio <romerixo@gmail.com>
2017-03-26 16:42:56 +0200

    some changes

--- color.py
+++ color.py
@@ -2,15 +2,9 @@ from colorama import init, Fore, Style
 
 init()
 
-WARN = Fore.YELLOW + Style.BRIGHT
+INFO = Fore.YELLOW + Style.BRIGHT
 OK = Fore.GREEN + Style.BRIGHT
 ERR = Fore.RED + Style.BRIGHT
 PROM = Fore.MAGENTA + Style.BRIGHT
 CLR = Fore.RESET + Style.RESET_ALL
 
-def ok(txt):
-    return OK + txt + CLR
-def err(txt):
-    return ERR + txt + CLR
-def prom(txt):
-    return PROM + txt + CLR
--- main.py
+++ main.py
@@ -1,53 +1,106 @@
-import sys
 import color as c
-from scapy.all import *
+print c.INFO + 'Loading modules...' + c.CLR
+import sys
+from scapy.all import sr, ARP
 
 GATEWAY = None
-TARGETS = None
+TARGET = None
+
+# TODO: target -> range, ip_list
+
+# ip in format 'xxx.xxx.xxx.xxx'
+def _ip_next(ip):
+    # convert in int
+    ip = ip.split('.')
+    for i, ip_sec in enumerate(ip):
+        ip[i] = int(ip_sec)
+
+    if(ip[3] < 255):
+        ip[3] += 1
+    elif(ip[2] < 255):
+        ip[3] = 1
+        ip[2] += 1
+    elif(ip[1] < 255):
+        ip[3] = 1
+        ip[2] = 1
+        ip[1] += 1
+    elif(ip[0] < 255):
+        ip[3] = 1
+        ip[2] = 1
+        ip[1] = 1
+        ip[0] += 1
+    
+    # convert to str
+    for i, ip_sec in enumerate(ip):
+        ip[i] = str(ip_sec)
+
+    return '.'.join(ip)
+    
+def _get_range(ip_range):
+    ips = []
+    
+    ip_start = ip_range.split('-')[0]
+    ip_end = ip_range.split('-')[1]
+
+    ips.append(ip_start)
+   
+    while(ips[-1] != ip_end):
+        ips.append(_ip_next(ips[-1]))
+
+    return ips
 
 def check_args():
-    global GATEWAY, TARGETS
-    argv = sys.argv[1:]
-    argc = len(argv)
+    global TARGET, GATEWAY
 
-    # gateway
-    if('-g' in argv):
-        index = argv.index('-g') + 1
-        if(index < argc):
-            GATEWAY = argv[index]
+    if(len(sys.argv) == 3):
+        GATEWAY = sys.argv[1]
 
-    # targets
-    if('-t' in argv):
-        index = argv.index('-t') + 1 
-        if(index < argc):
-            TARGETS = list(argv[index].split(','))
+        # target
+        for c in sys.argv[2]:
+            if(c == '-'):
+                TARGET = _get_range(sys.argv[2])
+                break
+            else:
+                TARGET = sys.argv[2].split(',')
 
-    return (GATEWAY and TARGETS)
+    return (GATEWAY and TARGET)
 
 def usage():
     print c.ERR + \
 '''
 Sintaxis error
 Usage:
-    venom_arp -g <gateway> -t <target_list>
+    venom_arp <ip_gateway> (<ip_target> | <ip_list> | <ip_range>)
 '''
 
 def get_mac(ip):
     global GATEWAY
-    result = sr(ARP(op=ARP.who_has, pdst=ip), verbose=False)
-    return result[0][ARP][0][1].hwsrc
+    mac = None
+    
+    try:
+        result = sr(ARP(op=ARP.who_has, pdst=ip), verbose=False, timeout=0.5)
+        mac = result[0][ARP][0][1].hwsrc
+    except:
+        pass
+
+    return mac
 
 ################################# main #########################################
 
 def main():
-    GATEWAY = '192.168.1.1'
-    print get_mac('192.168.1.128')
+    global TARGET, GATEWAY
+# Geting MAC for IP 192.168.1.100 ...
+    if(check_args()):
+        # try:
+        print c.PROM + '      IP                  MAC'
+        for ip in TARGET:
+            print c.OK + '{}      {}'.format(ip, get_mac(ip))
+        # except Exception as e:
+        #     print c.ERR + str(e)
 
-    # if(check_args()):
-    #     print get_mac()
-    # else:
-    #     usage()
-    #     sys.exit(1)
+    else:
+        usage()
+        return -1
 
 if(__name__ == '__main__'):
         main()
